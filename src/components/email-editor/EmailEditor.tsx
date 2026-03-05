3955589bc11abee7eec96fc396306d32e4cd9425
Vlad Syzov <hizes5050@gmail.com>
2026-03-05 01:02:31 +0100

    Bold&Italic&Underline tools is ready

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,23 +1,43 @@
 import { TextareaHTMLAttributes, useRef, useState } from 'react';
 import styles from './EmailEditor.module.scss';
 import { Bold, Eraser, Italic, Underline } from 'lucide-react';
+import { applyStyle, TStyle } from './apply-style';
+import parse from 'html-react-parser';
 
 export function EmailEditor() {
   const [text, setText] = useState(
     ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, animi eaque distinctio delectus voluptatibus reiciendis nemo accusantium exercitationem quibusdam architecto recusandae sunt maxime amet a sint perspiciatis in. Amet, obcaecati!',
   );
 
+  const [selectionStart, setSelectionStart] = useState(0);
+  const [selectionEnd, setSelectionEnd] = useState(0);
+
+  const updateSelection = () => {
+    if (!textRef.current) return;
+
+    setSelectionStart(textRef.current.selectionStart);
+    setSelectionEnd(textRef.current.selectionEnd);
+  };
+
   const textRef = useRef<HTMLTextAreaElement | null>(null);
 
-  const getSelectionText = () => {
-    if (!textRef) return;
-    const cursorStart = textRef.current?.selectionStart;
-    const cursorEnd = textRef.current?.selectionEnd;
-    let selectedText = text.substring(cursorStart, cursorEnd);
+  const applyFormat = (type: TStyle) => {
+    if (selectionStart === selectionEnd) {
+      console.log('Ничего не выделено');
+      return; // Ничего не выделено
+    }
 
-    if (!selectedText) return;
+    const selectedText = text.substring(selectionStart, selectionEnd);
+    const before = text.substring(0, selectionStart); // Текст ДО выделения
+    const after = text.substring(selectionEnd); // Текст ПОСЛЕ выделения
+    const formattedText = applyStyle(type, selectedText); // Форматированный текст
 
-    console.log(selectedText);
+    const newText = before + formattedText + after;
+    setText(newText);
+
+    console.log('Применен стиль:', type);
+    console.log('Было:', selectedText);
+    console.log('Стало:', formattedText);
   };
   return (
     <div>
@@ -27,7 +47,7 @@ export function EmailEditor() {
           ref={textRef}
           className={styles.editor}
           spellCheck="false"
-          onClick={getSelectionText}
+          onSelect={updateSelection}
           value={text}
           onChange={(e) => setText(e.target.value)}></textarea>
         <div className={styles.actions}>
@@ -35,13 +55,13 @@ export function EmailEditor() {
             <button onClick={() => setText('')}>
               <Eraser size={16} />
             </button>
-            <button>
+            <button onClick={() => applyFormat('bold')}>
               <Bold size={16} />
             </button>
-            <button>
+            <button onClick={() => applyFormat('italic')}>
               <Italic size={16} />
             </button>
-            <button>
+            <button onClick={() => applyFormat('underline')}>
               <Underline size={16} />
             </button>
           </div>
